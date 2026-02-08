5a6706881570d6eb8115af2f24227daa3ce9ab0d
thinkingGorilla <4zxzxz4@gmail.com>
2026-02-08 13:46:15 +0900

    useEffect 학습

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import Button from './Button';
 import styles from './App.module.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(0);
+    const onClick = () => setCount(previous => previous + 1);
+
+    const [keyword, setKeyword] = useState("")
+    const onChange = (e) => setKeyword(_ => e.target.value);
+
+    console.log("I run all the time.");
+    const isRunOnlyOnce = () => console.log("I run only once.");
+    useEffect(isRunOnlyOnce, []);
+    useEffect(
+        () => {
+            if (keyword.length !== "" && keyword.length > 5) {
+                console.log(`Search for ${keyword}`)
+            }
+        },
+        [keyword]
+    );
+    useEffect(
+        () => console.log("I run when keyword or count changes."),
+        [keyword, count]
+    );
+
     return (
         <div>
+            <input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder="Type something..."
+            />
             <h1 className={styles.title}>{count}</h1>
             <button onClick={onClick}>click me!</button>
         </div>
