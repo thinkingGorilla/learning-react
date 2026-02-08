33ee1753c1823922b74298983130fefd4e247315
thinkingGorilla <4zxzxz4@gmail.com>
2026-02-08 14:03:23 +0900

    주석 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import {useEffect, useState} from "react";
 
-// return이 없으면 undefined를 반환
+// 자바스크립트에서 return이 없으면 undefined를 반환
 function Hello() {
     useEffect(() => console.log("Hello"), []);
+    // effect 파라미터가 함수를 반환하는 경우 언마운트 시점에 해당 함수를 호출
     useEffect(() => () => console.log("Goodbye"), []);
 
     return <h1>Hello</h1>;
