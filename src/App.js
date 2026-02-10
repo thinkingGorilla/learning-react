b9864488df01413c7b292c193263a5b516f51e0b
thinkingGorilla <4zxzxz4@gmail.com>
2026-02-10 20:31:27 +0900

    To-Do 리스트 추가 및 불필요한 컴포넌트 제거

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import Button from './Button';
-import styles from './App.module.css';
 import {useState} from "react";
 
 function App() {
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    const onChange = (e) => setTodo(unused => e.target.value);
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (todo.trim().length === 0) return;
+
+        // reset the input field
+        setTodo("");
+        setTodos(current => [todo, ...current]);
+    }
+
     return (
         <div>
-            <h1 className={styles.title}>{count}</h1>
-            <button onClick={onClick}>click me!</button>
+            <h1>My Todos ({todos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange}
+                       type="text"
+                       value={todo}
+                       placeholder="Write your to do..."
+                />
+                <button>Add Todo</button>
+            </form>
         </div>
     );
 }
