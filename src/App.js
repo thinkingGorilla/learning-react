eba2fa675aedeaea7f1719473c1be5a50c91bb9a
thinkingGorilla <4zxzxz4@gmail.com>
2026-02-10 20:40:39 +0900

    To-Do 리스트 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
 
         // reset the input field
         setTodo("");
+        // add todo to the list
         setTodos(current => [todo, ...current]);
     }
 
@@ -25,6 +26,14 @@ function App() {
                 />
                 <button>Add Todo</button>
             </form>
+            <hr/>
+            <ul>
+                {
+                    // key는 React의 Virtual DOM diff 알고리즘을 위해 사용한다.
+                    // 실제 DOM에는 렌더링되지 않는다.
+                }
+                {todos.map((value, index) => <li key={index}>{value}</li>)}
+            </ul>
         </div>
     );
 }
