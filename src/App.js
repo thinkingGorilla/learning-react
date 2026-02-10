805b570c5252e5a77c26b8fd1abd34362d136524
thinkingGorilla <4zxzxz4@gmail.com>
2026-02-10 21:08:32 +0900

    코인 변환 및 API 데이터를 활용한 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import Button from './Button';
-import styles from './App.module.css';
-import {useState} from "react";
+import {useEffect, useState} from 'react';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    const [dollors, setDollors] = useState(0);
+
+    const onChange = (e) => setDollors(unused => Number(e.target.value));
+
+    console.log("rendered.");
+
+    useEffect(
+        () => {
+            fetch("https://api.coinpaprika.com/v1/tickers")
+                .then(response => response.json())
+                .then(json => {
+                    setCoins(unused => json);
+                    setLoading(false);
+                })
+        },
+        []
+    );
+
     return (
         <div>
-            <h1 className={styles.title}>{count}</h1>
-            <button onClick={onClick}>click me!</button>
+            <h1>The Coins! {!loading && <span>({coins.length})</span>}</h1>
+            {loading && <strong>Loading...</strong>}
+            <div>
+                <input type="number" value={dollors} onChange={onChange}/>
+                <span>dollars</span>
+            </div>
+            <ul>
+                {coins.map(coin => (
+                    <li key={coin.id}>
+                        {coin.name}
+                        ({coin.symbol}): {coin.quotes.USD.price.toFixed(3)} USD
+                        {dollors > 0 && (
+                            <span> - {(dollors / coin.quotes.USD.price).toFixed(6)} {coin.symbol}</span>
+                        )}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
